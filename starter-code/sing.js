ffc7c2f3e9db6d044f14a74ea0019eb8ec1b03fa
vedelopment <vedelopment@gmail.com>
2016-11-30 08:22:33 -0800

    fixes math/logic so that user input reflects 2 or 1 verse choices accurately

--- starter-code/sing.js
+++ starter-code/sing.js
@@ -4,9 +4,9 @@ var song = function() {
   for (x=verses; x>0; x--) {
     if (x>2) {
       console.log(x + " bottles of beer on the wall, "+x+" bottles of beer!Take one down and pass it around, "+(x-1)+" bottles of beer on the wall!");
-    } else if (x===2) {
+    } else if (x==2) {
       console.log(x + " bottles of beer on the wall, "+x+" bottles of beer!Take one down and pass it around, "+(x-1)+" bottle of beer on the wall!");
-    } else if (x===1) {
+    } else if (x==1) {
       console.log("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.");
       break;
     }
