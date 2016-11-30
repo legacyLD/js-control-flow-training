aa197ed1279aed409c6052cd64f80d7458d89ce2
vedelopment <vedelopment@gmail.com>
2016-11-30 07:11:49 -0800

    updates login.js to include authorization to eject warp core

--- starter-code/login.js
+++ starter-code/login.js
@@ -1,1 +1,18 @@
 console.log("login.js loaded");
+var userLogin = {userName: "Picard", userPassword: "teaearlgreyhot"}
+var login = function() {
+  if (userLogin){
+    for (i = 0; i < 3; i++) {
+      var ejectWarpCore = prompt("enter command authorization code for Picard to eject warp core")
+      if (ejectWarpCore===userLogin.userPassword){
+      alert("warp core ejection in progress, all hands brace for impact")
+      break;
+    } else if (i<2){
+      alert("please re-enter command authorization code for Picard to eject warp core")
+    } else if (i=2) {
+      alert("I'm sorry, you're all going to die now")
+      }
+    }
+  }
+}
+login();
