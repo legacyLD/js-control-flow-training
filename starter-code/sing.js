117b0e8c917f7ed1e40e2f68fc054b10b8d3363e
vedelopment <vedelopment@gmail.com>
2016-11-30 08:02:50 -0800

    adds prompt to ask user how drunk they want to get

--- starter-code/sing.js
+++ starter-code/sing.js
@@ -1,6 +1,7 @@
 console.log("sing.js loaded");
+var verses = prompt("How many verses of '99 Bottles Of Beer On The Wall' would you like to hear? (Please type as a numerical value an not a number word)");
 var song = function() {
-  for (x=5; x>0; x--) {
+  for (x=verses; x>0; x--) {
     if (x>2) {
       console.log(x + " bottles of beer on the wall, "+x+" bottles of beer!Take one down and pass it around, "+(x-1)+" bottles of beer on the wall!");
     } else if (x===2) {
